fe7aa023f778cfced68c363879b2b72f4fdab21d
mrmav <mrmav@outlook.pt>
2019-04-16 16:15:08 +0100

    boilerplate

--- p3d-tp/Main.cpp
+++ p3d-tp/Main.cpp
@@ -1,12 +1,44 @@
-#include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-int main() {
+#include <stdio.h>
 
-	std::cout << "Hello?" << std::endl;
+#pragma comment(lib, "glew32s.lib")
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "opengl32.lib")
 
-	int n;
-	std::cin >> n;
+void errorCallbackFunction(int error, const char* description) {
+	printf("ERROR %i: %s", error, description);
+}
 
+int main(void) {
+
+	GLFWwindow* window;
+
+	glfwSetErrorCallback(errorCallbackFunction);
+
+	if (!glfwInit()) return -1;
+
+	window = glfwCreateWindow(600, 540, "P3D-TP", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glClearColor(1.0, 0.0, 1.0, 1.0);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
 	return 0;
 
 }
