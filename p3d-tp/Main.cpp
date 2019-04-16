6f76ff84dafaaf2f1470c0e290c57b99dce9a1a2
mrmav <mrmav@outlook.pt>
2019-04-16 16:32:30 +0100

    more boilerplate

--- p3d-tp/Main.cpp
+++ p3d-tp/Main.cpp
@@ -2,35 +2,82 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <stdio.h>
-
 #pragma comment(lib, "glew32s.lib")
 #pragma comment(lib, "glfw3.lib")
 #pragma comment(lib, "opengl32.lib")
 
-void errorCallbackFunction(int error, const char* description) {
-	printf("ERROR %i: %s", error, description);
-}
+#include <iostream>
+
+void error_callback(int error, const char* description);
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+void process_input(GLFWwindow* window);
+
+
+int screenWidth = 800;
+int screenHeight = 600;
+float fov = 45.0f;
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+float lastX = screenWidth / 2, lastY = screenHeight / 2;
+float mouseYaw = 0.0f, mousePitch = 0.0f;
+bool firstMouse = true;
 
 int main(void) {
 
 	GLFWwindow* window;
 
-	glfwSetErrorCallback(errorCallbackFunction);
+	glfwSetErrorCallback(error_callback);
 
-	if (!glfwInit()) return -1;
-
-	window = glfwCreateWindow(600, 540, "P3D-TP", NULL, NULL);
-	if (!window) {
-		glfwTerminate();
+	if (!glfwInit()) {
+		
 		return -1;
+	
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(screenWidth, screenHeight, "P3D-TP", NULL, NULL);
+	
+	if (!window) {
+		
+		glfwTerminate();
+		
+		return -1;
+	
 	}
 
 	glfwMakeContextCurrent(window);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	std::cout << glGetString(GL_VERSION) << " " << glGetString(GL_VENDOR) << " " << glGetString(GL_RENDERER) << std::endl;
+
+	glViewport(0, 0, screenWidth, screenHeight);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	//glfwSetCursorPosCallback(window, mouse_callback);
+	//glfwSetScrollCallback(window, scroll_callback);
 
 	glClearColor(1.0, 0.0, 1.0, 1.0);
 
 	while (!glfwWindowShouldClose(window)) {
+
+		process_input(window);
+
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+
 		glClear(GL_COLOR_BUFFER_BIT);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -40,5 +87,32 @@ int main(void) {
 
 	glfwTerminate();
 	return 0;
+
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+
+	glViewport(0, 0, width, height);
+
+	screenWidth = width;
+	screenHeight = height;
+
+}
+
+void error_callback(int error, const char* description) {
+
+	std::cout << "ERROR " << error << ": " << description;
+
+}
+
+void process_input(GLFWwindow* window) {
+
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+
+		glfwSetWindowShouldClose(window, true);
+
+	}
+
 
 }
