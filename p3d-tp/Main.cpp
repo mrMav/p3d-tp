eeeda0033a5fd1183935413347e5101e3f70583e
mrmav <mrmav@outlook.pt>
2019-04-23 15:49:23 +0100

    added glm lib

--- p3d-tp/Main.cpp
+++ p3d-tp/Main.cpp
@@ -1,12 +1,17 @@
+#include <iostream>
+
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #pragma comment(lib, "glew32s.lib")
 #pragma comment(lib, "glfw3.lib")
 #pragma comment(lib, "opengl32.lib")
 
-#include <iostream>
 
 void error_callback(int error, const char* description);
 
@@ -17,7 +22,6 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 void process_input(GLFWwindow* window);
-
 
 int screenWidth = 800;
 int screenHeight = 600;
@@ -67,7 +71,7 @@ int main(void) {
 	//glfwSetCursorPosCallback(window, mouse_callback);
 	//glfwSetScrollCallback(window, scroll_callback);
 
-	glClearColor(1.0, 0.0, 1.0, 1.0);
+	glClearColor(0.1, 0.1, 0.1, 1.0);
 
 	while (!glfwWindowShouldClose(window)) {
 
@@ -76,6 +80,7 @@ int main(void) {
 		float currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
+
 
 
 		glClear(GL_COLOR_BUFFER_BIT);
